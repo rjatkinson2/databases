b1c2a12301c4e7381ee9efd6948443b5359cd303
rjatkinson2 <rjatkinson2@gmail.com>
2015-02-27 17:17:11 -0800

    Retrieve files correctly from database with curl command.

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -5,7 +5,14 @@ var bluebird = require('bluebird');
 
 module.exports = {
   messages: {
-    get: function (req, res) {}, // a function which handles a get request for all messages
+    get: function (req, res) {
+      console.log('hey we got here');
+      models.messages.get(function(err, rows, columns){
+        //gets the data somehow by passing it into callback
+        console.log(rows);
+        res.json(rows);
+      });
+    }, // a function which handles a get request for all messages
     post: function (req, res) {} // a function which handles posting a message to the database
   },
 
--- server/models/index.js
+++ server/models/index.js
@@ -5,7 +5,11 @@ var db = require('../db');
 
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
+    get: function (callback) {
+      db.query('select * from messages',function(err, rows, column){
+        callback(err, rows, column);
+      });
+    }, // a function which produces all the messages
     post: function () {} // a function which can be used to insert a message into the database
   },
 
