f1b865fcfe22703dadbd8498a801358675e9e502
rjatkinson2 <rjatkinson2@gmail.com>
2015-02-27 16:15:06 -0800

    Add functionality to connect server to mysql database. Additionally, add client files from previous chatter-box implementation.

--- server/db/index.js
+++ server/db/index.js
@@ -3,5 +3,13 @@ var mysql = require('mysql');
 // Create a database connection and export it from this file.
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
+var connection = mysql.createConnection({
+  user: 'root',
+  password: '', //try undefined if this doesn't work
+  database: 'chat',
+  host: 'localhost'
+});
+
+module.exports = connection; //consumer will need to call connect()
 
 
